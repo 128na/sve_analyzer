047b19da120d4339d973a643b599f7f4a0b7ed77
shingoushori <wataruowaki@owakiwatarunoMacBook-Pro.local>
2019-12-15 18:45:10 +0900

    路線情報/駅情報/プレイヤースロット

--- src/services/simutrans.js
+++ src/services/simutrans.js
@@ -25,11 +25,13 @@ export default {
     this.mergeStationInfo();
     this.mergePlayerInfo();
 
+    console.log(this.data);
     return {
       simutrans: this.data.simutrans,
       map: this.data.map,
       stations: this.data.stations,
       players: this.data.players,
+      lines: this.data.lines,
     };
   },
   data: null,
@@ -41,6 +43,7 @@ export default {
       stations_names: [],
       player_types: [],
       players: [],
+      lines: [],
     }
   },
   createParser(resolved, reject) {
@@ -82,6 +85,9 @@ export default {
       this.parseStationNames(parser);
       this.parsePlayerTypes(parser);
       this.parsePlayers(parser);
+      this.parsePlayerSettings(parser);
+      this.parseHaltInfos(parser);
+      this.parseLines(parser);
       stream.pipe(parser);
     });
   },
@@ -335,9 +341,324 @@ export default {
 
 
   },
-  // プレイヤー情報にプレイヤータイプを統合
+  parsePlayerSettings (parser) {
+    let is_einstellungen = false;
+    let count_einstellungen = 0;
+    let count_einstellungen_slot = 0;
+    const playerIdToSlot = [];
+    this.data.player_settings = {};
+    
+    parser.onopenTagName('einstellungen_t', () => {
+        is_einstellungen = true;
+    });
+    parser.oncloseTagName('einstellungen_t', () => {
+        is_einstellungen = false;
+        this.data.player_settings.slot = playerIdToSlot;
+    });
+    parser.on('text', text => {
+        if (is_einstellungen) {
+          // console.log(count, text);
+          let act = false;
+          switch (count_einstellungen) {
+            case 70:
+            case 72:
+            case 74:
+            case 76:
+            case 78:
+            case 80:
+            case 82:
+            case 84:
+            case 86:
+            case 88:
+            case 90:
+            case 92:
+            case 94:
+            case 96:
+            case 98:
+            case 100:
+            if(parseInt(text, 10) == 1) {
+              playerIdToSlot.push(count_einstellungen_slot);
+            }
+            count_einstellungen_slot++;
+            break;
+          }
+          count_einstellungen++;
+        }
+    });
+  },
+  // プレイヤー情報に追加情報を統合
   mergePlayerInfo() {
+    // プレイヤータイプを統合
     this.data.players = this.data.players.map((p, i) => Object.assign(
       { id: i }, p, this.data.player_types[i]));
+    // マップ設定から取得できるプレイヤー情報を統合
+    this.data.players = this.data.players.map((p, i) => Object.assign(
+      { slot: this.data.player_settings.slot[i] }, p));
+    console.log(this.data.players);
   },
+  
+  // -----------------------
+  // 駅情報 ... 統計情報をとるならこちら
+  parseHaltInfos (parser) {
+    let halt = {};
+    let x, y, z;
+    
+    let is_halt = false;
+    let text_count = 0;
+    let is_koord3d = false;
+    let koord3d_count = 0;
+    let text_koord3d_count = 0;
+    
+    // 各駅
+    let halt_count = 0;
+    parser.on('opentag', el => {
+        if (el.name === 'haltestelle_t') {
+          is_halt = true;
+          text_count = 0;
+          koord3d_count = 0;
+          halt.ground = [];
+        }
+    });
+    parser.on('closetag', name => {
+        if (name === 'haltestelle_t') {
+          is_halt = false;
+          halt_count++;
+          halt = {}
+        }
+    });
+    
+    
+    // ground
+      parser.on('opentag', el => {
+        if (is_halt) {
+          if (el.name === 'koord3d') {
+            is_koord3d = true;
+            text_koord3d_count = 0;
+          }
+        }
+      });
+      parser.on('closetag', name => {
+        if (is_halt) {
+          if (name === 'koord3d') {
+            is_koord3d = false;
+            koord3d_count++;
+            x = -1;
+            y = -1;
+            z = -1;
+          }
+        }
+      });
+      parser.on('text', text => {
+        if (is_halt && is_koord3d) {
+          if (text_koord3d_count === 0) {
+            x = parseInt(text, 10);
+          }
+          else if (text_koord3d_count === 1) {
+            y = parseInt(text, 10);
+          }
+          else if (text_koord3d_count === 2) {
+            z = parseInt(text, 10);
+            
+            if ( x != -1 && y != -1 && z != -1 ) {
+              // [-1, -1, -1] は除外 おそらくスペーサー
+              halt.ground.push([x, y, z]);
+              if (koord3d_count === 0) {
+                halt.coordinate = [x, y, z];
+              }
+            }
+          }
+          text_koord3d_count++;
+        }
+      });
+      
+      // owner
+      parser.on('text', text => {
+        if (is_halt && !is_koord3d) {
+          if (text_count === 0) {
+            halt.no = parseInt(text, 10);
+          }
+          else if (text_count === 1) {
+            halt.owner = parseInt(text, 10);
+          }
+          text_count++;
+        }
+      });
+    },
+    
+  
+  // -----------------------
+  // 路線情報
+  /*lines:[
+  {
+    id:(int)路線ID,
+    player_id:(int)会社ID,
+    name:(str)路線名,
+    type:(str?int?)鉄道とかの種別
+    stops: [[x,y,z], ...],
+  },
+  ...
+  ]*/
+  parseLines(parser) {
+    let line = {};
+    let x, y, z;
+    
+    
+    let text_count = 0;
+    let is_koord3d = false;
+    let koord3d_count = 0;
+    let text_koord3d_count = 0;
+    
+    // 会社ごと
+    let is_player = false;
+    let player_count = 0;
+    let player_name;
+    
+    parser.on('opentag', el => {
+        if (el.name === 'spieler_t') {
+          is_player = true;
+          text_count = 0;
+          koord3d_count = 0;
+        }
+    });
+    parser.on('closetag', name => {
+        if (name === 'spieler_t') {
+          is_player = false;
+          player_count++;
+        }
+    });
+    
+    // 路線ごと
+    /*
+  <simlinemgmt_t>
+   <i32>その会社の路線数</i32>
+   <i32>↓の路線のタイプ</i32>
+   <simline_t>
+    <![CDATA[路線名]]>
+    <i16>路線のId</i16>
+    <fahrplan_t>
+    ...
+    </fahrplan_t>
+    ...
+   </simline_t>
+   <i32>↓の路線のタイプ</i32>
+   <simline_t>
+   </simline_t>
+   ...
+  </simlinemgmt_t>
+    */
+    let is_line = false;
+    let line_count = 0;
+    let test_count_line = 0;
+    parser.on('opentag', el => {
+        if (el.name === 'simline_t') {
+          is_line = true;
+          test_count_line = 0;
+          koord3d_count = 0;
+          line = {}
+          line.player_id = player_count;
+          line.type = parseInt(latest_text, 10);
+          line.stops = [];
+        }
+    });
+    parser.on('closetag', name => {
+        if (name === 'simline_t') {
+          is_line = false;
+          this.data.lines.push(line);
+          line_count++;
+          line = {};
+        }
+    });
+    // 路線名
+    parser.on('cdata', text => {
+        if (is_line) {
+          line.name = text;
+        }
+    });
+    
+    
+    let is_linemgmt = false;
+    parser.on('opentag', el => {
+        if (el.name === 'simlinemgmt_t') {
+          is_linemgmt = true;
+        }
+    });
+    parser.on('closetag', name => {
+        if (name === 'simlinemgmt_t') {
+          is_linemgmt = false;
+        }
+    });
+    // 路線タイプが付与対象lineの取得もと<simline_t>の直前の要素にいる都合
+    let latest_text;
+    parser.on('text', text => {
+        if (!is_line && is_linemgmt) {
+          latest_text = text;
+        }
+        if (is_line && is_linemgmt) {
+          latest_text = text;
+          if (test_count_line == 0) {
+            line.id = parseInt(latest_text, 10);
+          }
+          test_count_line++;
+        }
+    });
+    
+    // 駅
+    let is_stop = false;
+    let stop_count = 0;
+    let text_count_stop = 0;
+    parser.on('opentag', el => {
+        if (el.name === 'fahrplan_t') {
+          is_stop = true;
+          text_count_stop = 0;
+          koord3d_count = 0;
+          line.stops = [];
+        }
+    });
+    parser.on('closetag', name => {
+        if (name === 'fahrplan_t') {
+          is_stop = false;
+          stop_count++;
+        }
+    });
+    // koord3d
+    parser.on('opentag', el => {
+        if (is_stop) {
+          if (el.name === 'koord3d') {
+            is_koord3d = true;
+            text_koord3d_count = 0;
+          }
+        }
+    });
+    parser.on('closetag', name => {
+        if (is_stop) {
+          if (name === 'koord3d') {
+            is_koord3d = false;
+            koord3d_count++;
+            x = -1;
+            y = -1;
+            z = -1;
+          }
+        }
+    });
+    parser.on('text', text => {
+        if (is_stop && is_koord3d) {
+          if (text_koord3d_count === 0) {
+            x = parseInt(text, 10);
+          }
+          else if (text_koord3d_count === 1) {
+            y = parseInt(text, 10);
+          }
+          else if (text_koord3d_count === 2) {
+            z = parseInt(text, 10);
+            
+            line.stops.push([x, y, z]);
+              
+          }
+          text_koord3d_count++;
+        }
+    });
+    
+  },
+  
+
 }
