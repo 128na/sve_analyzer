2d84654b65bb1f5c4acfb0cfc2e38c4ee1b5d249
shingoushori <wataruowaki@owakiwatarunoMacBook-Pro.local>
2019-12-22 20:41:21 +0900

    player_color インデント修正

--- src/services/parsers/players_parser.js
+++ src/services/parsers/players_parser.js
@@ -1,5 +1,5 @@
 export default {
-  
+
   // プレイヤー情報
   parse(parser, result) {
     result.players = [];
@@ -8,43 +8,41 @@ export default {
     let player = {};
     let text_count = 0;
     parser.onopenTagName('spieler_t', () => {
-        is_player = true;
-        text_count = 0;
+      is_player = true;
+      text_count = 0;
     });
     parser.oncloseTagName('spieler_t', () => {
-        is_player = false;
-        result.players.push(player);
-        player = {};
-        console.log(result.players);
+      is_player = false;
+      result.players.push(player);
+      player = {};
     });
     parser.onopenTagName('simline_t', () => {
-        is_line = true;
+      is_line = true;
     });
     parser.oncloseTagName('simline_t', () => {
-        is_line = false;
+      is_line = false;
     });
-    
+
     parser.on('cdata', text => {
-        if (is_player && !is_line) {
-          player.name = text;
-        }
+      if (is_player && !is_line) {
+        player.name = text;
+      }
     });
-    
-    
+
     parser.on('text', text => {
-        if (is_player && !is_line) {
-          switch (text_count) {
-            // player/simplay.h
-            // class player_t の変数としては uint8 player_color_1, player_color_2;
-            case 0 :
-            player.color_1 = parseInt(text, 10); // 基調色
-            break;
-            case 1 :
-            player.color_2 = parseInt(text, 10); // 補助色
-            break;
-          }
-          text_count++;
+      if (is_player && !is_line) {
+        switch (text_count) {
+          // player/simplay.h
+          // class player_t の変数としては uint8 player_color_1, player_color_2;
+          case 0 :
+          player.color_1 = parseInt(text, 10); // 基調色
+          break;
+          case 1 :
+          player.color_2 = parseInt(text, 10); // 補助色
+          break;
         }
+        text_count++;
+      }
     });
     
   },
