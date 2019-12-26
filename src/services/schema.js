b2bbf48067b9c025772bfd27fbb0366a2f45dd7e
128na <simutrans.128na@gmail.com>
2019-12-26 14:12:07 +0900

    バージョンを更新

--- src/services/schema.js
+++ src/services/schema.js
@@ -1,8 +1,11 @@
 import schema_v1 from '../schemas/v1.json';
+import schema_v1_2 from '../schemas/v1.2.json';
 
 export default {
   get(version) {
-    // console.log('app version : ', version);
+    if (version >= '1.2') {
+      return schema_v1_2;
+    }
 
     return schema_v1;
   }
